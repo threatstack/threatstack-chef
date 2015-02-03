4e87bac5e1a7d19b05692d304bf499acd8b1678a
Pete Cheslock <petecheslock@gmail.com>
2015-02-02 20:21:54 -0500

    Release v1.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@threatstack.com'
 license          'Apache 2.0'
 description      'Installs/Configures Threat Stack cloudsight components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
 supports 'amazon'
 supports 'centos'
