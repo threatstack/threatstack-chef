72551aeb607716a8579577aeec2e91642408d2fd
Pete Cheslock <petecheslock@gmail.com>
2015-08-27 14:03:03 -0400

    Cleanup tests and fix tests for changes to cookbook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 # spec_helper.rb
 require 'chefspec'
+
+RSpec.configure do |config|
+  config.platform = 'ubuntu'
+  config.version = '14.04'
+end
+
 ChefSpec::Coverage.start!
 require 'chefspec/berkshelf'
