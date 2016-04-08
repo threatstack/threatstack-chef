8fb2fc24a94d79de7dccbd6b6faf5d4f8046062d
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-04-08 18:25:35 -0400

    Handle CentOS 6 and curl issues with TLS > 1.0

--- recipes/rhel.rb
+++ recipes/rhel.rb
@@ -16,9 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+execute 'ts-gpg-fetch' do
+  command "wget #{node['threatstack']['repo']['key']} -O #{node['threatstack']['repo']['key']}"
+  creates node['threatstack']['repo']['key_file_url']
+end
+
 yum_repository 'threatstack' do
   description 'Threat Stack'
   baseurl node['threatstack']['repo']['url']
-  gpgkey node['threatstack']['repo']['key']
+  gpgkey node['threatstack']['repo']['key_file_url']
   action :add
 end
