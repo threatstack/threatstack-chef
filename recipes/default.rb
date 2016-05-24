d084c73d8212d760c0d35ca964330fdf1a4c705f
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-05-24 16:51:32 -0400

    revert back to `cloudsight stop` instead of `service cloudsight stop` to make tests pass.

--- recipes/default.rb
+++ recipes/default.rb
@@ -79,7 +79,7 @@ if ! node['threatstack']['rulesets'].empty?
 
     # Only if we are about to reconfigure a running instance
     execute 'stop threatstack services' do
-      command 'service cloudsight stop'
+      command '/usr/bin/cloudsight stop'
       action :nothing
       subscribes :run, 'file[/opt/threatstack/etc/active_rulesets.txt]', :immediately
     end
