534c5bc93ad6058e923829370428ddadeee89b08
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-05-25 17:41:18 -0400

    release 1.6.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@threatstack.com'
 license          'Apache 2.0'
 description      'Installs/Configures Threat Stack cloudsight components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.6.6'
+version          '1.6.7'
 
 supports 'amazon'
 supports 'centos'
