cfd0e9c200c1af95b764ac1b7b2153034e0fa3d8
Pete Cheslock <petecheslock@gmail.com>
2016-07-25 11:22:51 -0400

    Fix foodcritic errors

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures Threat Stack cloudsight components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.6.8'
+issues_url       'https://github.com/threatstack/threatstack-chef/issues'
+source_url       'https://github.com/threatstack/threatstack-chef'
 
 supports 'amazon'
 supports 'centos'
