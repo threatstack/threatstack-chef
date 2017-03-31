310f701b1e7a57d17ea1a86cd54b67fd95f77217
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-31 18:09:19 -0400

    Release 1.7.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@threatstack.com'
 license          'Apache 2.0'
 description      'Installs/Configures Threat Stack cloudsight components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.7.1'
+version          '1.7.2'
 issues_url       'https://github.com/threatstack/threatstack-chef/issues'
 source_url       'https://github.com/threatstack/threatstack-chef'
 
