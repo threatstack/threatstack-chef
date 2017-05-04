16513e89ca1d4e15d634af30a19a4a319c18c2ee
Ed Flanagan <ed@index.com>
2017-05-03 20:49:38 -0700

    Update metadata
    * Add respond_to? conditional for issues/source urls
    * Add minimum chef version

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures Threat Stack cloudsight components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.7.3'
-issues_url       'https://github.com/threatstack/threatstack-chef/issues'
-source_url       'https://github.com/threatstack/threatstack-chef'
+issues_url       'https://github.com/threatstack/threatstack-chef/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/threatstack/threatstack-chef' if respond_to?(:source_url)
 
 supports 'amazon'
 supports 'centos'
@@ -15,3 +15,5 @@ supports 'ubuntu'
 
 depends  'apt'
 depends  'yum'
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
