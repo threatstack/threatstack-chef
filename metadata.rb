fcd1dbceacaacb3b4471610d08c04f2b1baea7cb
Pete Cheslock <petecheslock@gmail.com>
2017-07-07 12:23:46 -0400

    Apt and yum resources are in chef > 12.15

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,16 @@
 name             'threatstack'
 maintainer       'Threat Stack'
 maintainer_email 'support@threatstack.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures Threat Stack cloudsight components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.7.3'
-issues_url       'https://github.com/threatstack/threatstack-chef/issues'
-source_url       'https://github.com/threatstack/threatstack-chef'
+version          '2.0.0'
+issues_url       'https://github.com/threatstack/threatstack-chef/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/threatstack/threatstack-chef' if respond_to?(:source_url)
 
 supports 'amazon'
 supports 'centos'
 supports 'redhat'
 supports 'ubuntu'
 
-depends  'apt'
-depends  'yum'
+chef_version '>= 12.15' if respond_to?(:chef_version)
