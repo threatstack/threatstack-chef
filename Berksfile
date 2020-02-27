25c103d37d5a072f106b8da44b69e7256ba60cf4
Michael Chmielewski <michael.chmielewski@threatstack.com>
2020-02-27 13:29:06 -0500

    Added new testing cookbook to support testing an upgrade from one agent version to another
    NOTE: For what appears to be a timing issue, the ubuntu 16/18 upgrade test runs fail locally on my machine.

--- Berksfile
+++ Berksfile
@@ -3,3 +3,4 @@ source 'https://supermarket.getchef.com'
 metadata
 
 cookbook 'setup', path: './test/cookbooks/setup', group: :integration
+cookbook 'install_old_agent', path: './test/cookbooks/install_old_agent', group: :integration
---
+++ test/cookbooks/install_old_agent/recipes/default.rb
@@ -0,0 +1,77 @@
+#
+# Cookbook Name:: install_old_agent
+# Recipe:: default
+#
+# Copyright 2014-2020, Threat Stack
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Just install old agent, don't fire it up.
+if node['threatstack']['repo_enable']
+  if platform_family?('rhel', 'fedora', 'amazon')
+    remote_file node['threatstack']['repo']['key_file'] do
+      source node['threatstack']['repo']['key']
+      owner 'root'
+      group 'root'
+      mode '0644'
+      action :create
+    end
+
+    yum_repository 'threatstack' do
+      description 'Threat Stack Package Repository'
+      baseurl node['threatstack']['repo']['url']
+      gpgkey node['threatstack']['repo']['key_file_uri']
+      gpgcheck node['threatstack']['validate_gpg_key']
+      action :add
+    end
+  else
+    package 'apt-transport-https'
+
+    apt_repository 'threatstack' do
+      uri node['threatstack']['repo']['url']
+      distribution node['threatstack']['repo']['dist']
+      components node['threatstack']['repo']['components']
+      key node['threatstack']['repo']['key']
+      action :add
+    end
+  end
+end
+
+execute 'stop_auditd' do # ~FC004 This is a workaround for auditd not stoppable with the standard method
+  command 'service auditd stop'
+  only_if { platform?('amazon') && node['platform_version'] == '2' || platform?('centos', 'redhat') }
+end
+
+execute 'disable_auditd' do
+  command 'systemctl disable auditd'
+  only_if { platform?('amazon') && node['platform_version'] == '2' || platform?('centos', 'redhat') }
+end
+
+# Change this as more agent versions are released, in accordance with official support
+# Hacky version logic to deal with debian systems
+package 'threatstack-agent' do
+  version platform_family?('rhel', 'fedora', 'amazon') ? node['threatstack']['old_version'] : node['threatstack']['old_version'] + '*'
+  action :install
+  notifies :run, 'execute[stop_auditd]', :before
+  notifies :run, 'execute[disable_auditd]', :before
+end
+
+# How we can verify what is installed is what is defined to be installed
+# See: http://www.hurryupandwait.io/blog/accessing-chef-node-attributes-from-kitchen-tests
+ruby_block "Save node attributes" do
+  block do
+    if Dir::exist?('/tmp/')
+      IO.write("/tmp/chef_node_for_tests.json", node.to_json)
+    end
+  end
+end
