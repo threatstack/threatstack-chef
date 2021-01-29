5febf853a1c397ff35597658275f450d0c10b1c2
Patrick Cable <pat.cable@threatstack.com>
2021-01-29 10:49:49 -0500

    Fix linter issues

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -18,23 +18,23 @@
 
 def ts_down?
   require 'open3'
-  stdout, stderr, status = Open3.capture3('/usr/bin/tsagent status')
-  return !stdout.include?('UP Threat Stack Backend Connection')
+  stdout, = Open3.capture3('/usr/bin/tsagent status')
+  !stdout.include?('UP Threat Stack Backend Connection')
 end
 
 def stale_agent?
   require 'open3'
   require 'yaml'
   require 'date'
-  stdout, stderr, status = Open3.capture3('/usr/bin/tsagent info')
+  stdout, = Open3.capture3('/usr/bin/tsagent info')
   stdout.gsub!("\t", '  ')
-  tsagent_info = YAML.load(stdout)
+  tsagent_info = YAML.safe_load(stdout)
 
   # If it's never been registered, it's stale.
   return true if tsagent_info['LastBackendConnection'] == 'N/A'
 
   # It could be stale if it last connected >24h ago. Would need to get ts_down? to know for sure.
-  return true if (DateTime.now.to_time - tsagent_info['LastBackendConnection'])/3600 > 24.0
+  return true if (DateTime.now.to_time - tsagent_info['LastBackendConnection']) / 3600 > 24.0
 
   # Otherwise, it's definitely fresh.
   false
